ad74448d1946a912993d25c01a96b5f0f9602a00
Cindy Mullins <mullins.cindy@gmail.com>
2019-05-17 20:23:55 -0700

    Create components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+// Header with app title, search bar
+const Header = () => {
+  return (
+    <header className="main-nav">
+
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <ul>
+      <li><a href='#'>Skyscrapers</a></li>
+      <li><a href='#'>Honeycombs</a></li>
+      <li><a href='#'>Corals</a></li>
+    </ul>
+  );
+}
+
+export default Nav;
